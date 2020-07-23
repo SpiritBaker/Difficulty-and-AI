4dbd28067c7914ecab84ea289b8e838d6205f49e
SpiritBaker <34450337+Ivanjpeg@users.noreply.github.com>
2020-07-23 20:12:58 +0600

    Adding custom Ruler traits
    Adding loan condition for AI personalities
    Creating possibility for AI Rev Empire
    Adding custom Ruler traits, wip description needed
    New AI logic for protect trade edict
    Enabling decision to form Egypt for AI
    Various small AI adjustments, localization

--- Difficulty and AI changes Full version/common/defines/00_AI_changes.lua
+++ Difficulty and AI changes Full version/common/defines/00_AI_changes.lua
@@ -22,15 +22,9 @@ NDefines.NEconomy.CARAVAN_POWER_MAX = 15
 
 NDefines.NAI.SUBSIDY_YEARS = 20
 NDefines.NAI.MIN_INCOME_FOR_SUBSIDIES = 50
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_WAR_WITH_RIVAL_FACTOR = 15	--AI scoring for giving subsidies to a country that is at war with its rival.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_POWERBALANCE_FACTOR = 15 --AI scoring to give subsidies to nations blocking/fighting power balance threat.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_RELATIONS_MAX = 200 --Relations above this will not be considered for DIPLOMATIC_ACTION_SUBSIDIES_RELATIONS_FACTOR.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_PREVIOUS_SUBSIDIES_FACTOR = -100 --AI scoring for giving subsidies based on previous subsidies nation is receiving relative to income.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_RELATIVE_INCOME_FACTOR	= 25 --AI scoring for giving subsidies to a country based on its relative income to the AI nation (lower income = higher scoring).
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_RELATIONS_FACTOR = 0.2	--AI scoring for giving subsidies to a country based on opinion of the other country.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_MAX_RELATIVE_INCOME = 0.2	--Max relative monthly income that a country can have for the AI to consider it in need of subsidies.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_INDEBTED_FACTOR = 25 --AI scoring for giving subsidies to an ally in heavy debt.
-NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_MIN_VALUE = 0.2 --Minimum amount of subsidies that AI considers worthwhile.
+
+
+
 NDefines.NAI.MAX_CAV_PERCENTAGE = 50
 NDefines.NAI.EXTRA_SURPLUS_WHEN_NEEDING_BUILDINGS = 0.15	--AI will aim for having at least this fraction of their income as additional surplus when they need buildings
 NDefines.NAI.ARMY_BUDGET_FRACTION = 0.7
@@ -45,15 +39,31 @@ NDefines.NAI.MIN_SHIPS_FOR_ADMIRAL = 25
 NDefines.NAI.DEVELOPMENT_CAP_BASE = 100 -- AI will not develop provinces that have more development than this or DEVELOPMENT_CAP_MULT*original development (whichever is bigger)
 NDefines.NAI.DEVELOPMENT_CAP_MULT = 10
 NDefines.NAI.EDICT_VALUE_THRESHOLD = 20
+NDefines.NAI.EDICT_VALUE_THRESHOLD_MULTIPLY_DEFICIT	= 2	--Change to above threshold in case of running deficit
+NDefines.NAI.EDICT_VALUE_THRESHOLD_MULTIPLY_LOW_INCOME = 1.5	--Change to above threshold in case of low income
 NDefines.NAI.CALL_IN_ALLIES_POWER_RATIO = 2.5
+NDefines.NAI.AI_WANT_ACCEPT_CULTURES = 300	--How much the AI values having much development in accepted cultures.
+NDefines.NAI.DEFICIT_SPENDING_MIN_MONTHS_PEACETIME = 12	--Same as DEFICIT_SPENDING_MIN_MONTHS, but during peacetime, no rebels and no war exhaustion
+
 NDefines.NAI.DIPLOMATIC_ACTION_GUARANTEE_POWERBALANCE_FACTOR = 25
 NDefines.NAI.POWERFUL_ALLY_PENALTY = 60	--Penalty on alliance for them already having a powerful ally if much stronger.
-NDefines.NAI.DIPLOMATIC_ACTION_VASSALIZE_DEVELOPMENT_FACTOR	= 50	--AI scoring for vassalize is increased by this for each development in target's provinces.
+NDefines.NAI.DIPLOMATIC_ACTION_VASSALIZE_DEVELOPMENT_FACTOR	= 25	--AI scoring for vassalize is increased by this for each development in target's provinces.
 NDefines.NAI.DIPLOMATIC_INTEREST_DISTANCE = 100
 NDefines.NAI.DIPLOMATIC_ACTION_MILITARY_ACCESS_EXISTING_RELATION_MULT = 5.0	--AI scoring for military access is multiplied by this if it has an existing power cost relation.
 NDefines.NAI.DIPLOMATIC_ACTION_MILITARY_ACCESS_ENEMY_REGIMENTS_FACTOR = 25.0	--AI scoring for military access is increased by this for each enemy regiment they are sheltering.
 NDefines.NAI.OVER_FORCELIMIT_AVOIDANCE_FACTOR = 2 --The higher this number is the less willing the AI will be to exceed forcelimits.
 NDefines.NAI.CHARTER_COMPANY_BASE_RELUCTANCE = -5
+
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_WAR_WITH_RIVAL_FACTOR = 15	--AI scoring for giving subsidies to a country that is at war with its rival.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_POWERBALANCE_FACTOR = 15 --AI scoring to give subsidies to nations blocking/fighting power balance threat.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_RELATIONS_MAX = 200 --Relations above this will not be considered for DIPLOMATIC_ACTION_SUBSIDIES_RELATIONS_FACTOR.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_PREVIOUS_SUBSIDIES_FACTOR = -100 --AI scoring for giving subsidies based on previous subsidies nation is receiving relative to income.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_RELATIVE_INCOME_FACTOR	= 25 --AI scoring for giving subsidies to a country based on its relative income to the AI nation (lower income = higher scoring).
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_RELATIONS_FACTOR = 0.2	--AI scoring for giving subsidies to a country based on opinion of the other country.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_MAX_RELATIVE_INCOME = 0.2	--Max relative monthly income that a country can have for the AI to consider it in need of subsidies.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_INDEBTED_FACTOR = 25 --AI scoring for giving subsidies to an ally in heavy debt.
+NDefines.NAI.DIPLOMATIC_ACTION_SUBSIDIES_MIN_VALUE = 0.2 --Minimum amount of subsidies that AI considers worthwhile.
+
 --NDefines.NAI.CHARTER_COMPANY_DEVELOPMENT_RELUCTANCE = 3
 NDefines.NAI.DEBASE_THRESHOLD = -5000 -- AI will not debase if it has more gold than this.
 NDefines.NAI.INVADING_BRAVERY =	1.0	--If (defender strength) / (invader strength) >
@@ -85,7 +95,7 @@ NDefines.NAI.PEACE_TERMS_ANNUL_TREATIES_BASE_MULT =	1
 NDefines.NAI.PEACE_TERMS_GOLD_BASE_MULT = 0.5
 NDefines.NAI.PEACE_TERMS_RELEASE_ANNEXED_MAX_MULT =	0.5	--Max AI desire mult for releasing countries.
 NDefines.NAI.PEACE_TERMS_WAR_REPARATIONS_BASE_MULT = 0.75	--AI desire for war reparations through peace.
-NDefines.NAI.PEACE_TERMS_WAR_REPARATIONS_MIN_INCOME_RATIO = 0.2 -- AI only wants war reparations if other country has at least this % of their income
+NDefines.NAI.PEACE_TERMS_WAR_REPARATIONS_MIN_INCOME_RATIO = 0.35 -- AI only wants war reparations if other country has at least this % of their income
 NDefines.NAI.PEACE_TERMS_PROVINCE_NO_INTEREST_MULT = 0.15  --AI desire for a province is multiplied by this if it is not on their conquest list.
 NDefines.NAI.PEACE_TERMS_PROVINCE_CORE_MULT = 5.0 -- AI desire for a province is multiplied by this if it has a core on it
 NDefines.NAI.PEACE_TERMS_RELEASE_ANNEXED_HRE_MULT = 3.0 -- AI desire for releasing a country is multiplied by this if both are HRE members
@@ -109,19 +119,19 @@ NDefines.NAI.REPAY_LOAN_BASE_AI_DESIRE = 100	-- AI scoring for repaying loans, m
 --NDefines.NAIEconomy.REBEL_THREAT_MILITARIZE_THRESHOLD = 0.8					-- Above what rebel threat should the AI militarize (i.e. focus spending on armies)
 --MILITARY_FOCUS_DEFAULT = 1.0
 --MILITARY_FOCUS_LOWER_BOUND = 0.0
---MILITARY_FOCUS_UPPER_BOUND = 2.0
+NDefines.NAIEconomy.MILITARY_FOCUS_UPPER_BOUND = 3.0
 NDefines.NAIEconomy.ARMY_FRACTION_MILITARIZE = 1.35
 NDefines.NAIEconomy.ARMY_FRACTION_PEACEFUL = 0.85
 NDefines.NAIEconomy.ARMY_FRACTION_MILITARIST = 1.15
 
-NDefines.NAIEconomy.NAVY_FRACTION_PEACEFUL = 1.0
-NDefines.NAIEconomy.NAVY_FRACTION_CAPITALIST = 1.15
+--NDefines.NAIEconomy.NAVY_FRACTION_PEACEFUL = 1.25
+--NDefines.NAIEconomy.NAVY_FRACTION_CAPITALIST = 1.25
 
 NDefines.NAIEconomy.FORT_FRACTION_MILITARIZE = 1.5
 NDefines.NAIEconomy.FORT_FRACTION_MILITARIST = 1.25
 NDefines.NAIEconomy.FORT_FRACTION_CAPITALIST = 0.25
 --MISSIONARY_FRACTION = 0.15
---STATE_MAINTENANCE_FRACTION = 0.1
+NDefines.NAIEconomy.STATE_MAINTENANCE_FRACTION = 0.25
 NDefines.NAIEconomy.LOAN_REPAYMENT_SAVINGS_PRIORITY = 2
 --SUBSIDY_PRIORITY_CONSIDERATION_THRESHOLD = 50.0
 --SUBSIDY_DESIRE_TO_PRIORITY_RATIO = 0.05
