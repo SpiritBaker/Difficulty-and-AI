b2b3de5fd770fa648a030eaed01bf1111b1935d0
SpiritBaker <34450337+Ivanjpeg@users.noreply.github.com>
2020-08-24 12:29:08 +0600

    Balancing war enthusiasm values
    Another iteration of debt spiral work around
    Balancing war enthusiasm values.
    Small ai will do changes for loan privileges and Idea groups
    Adjusting Rev spread and Rev center spawn.

--- Difficulty and AI changes Full version/common/defines/00_AI_changes.lua
+++ Difficulty and AI changes Full version/common/defines/00_AI_changes.lua
@@ -45,7 +45,7 @@ NDefines.NAI.EDICT_VALUE_THRESHOLD_MULTIPLY_DEFICIT	= 2	--Change to above thresh
 NDefines.NAI.EDICT_VALUE_THRESHOLD_MULTIPLY_LOW_INCOME = 1.5	--Change to above threshold in case of low income
 NDefines.NAI.CALL_IN_ALLIES_POWER_RATIO = 2.5
 NDefines.NAI.AI_WANT_ACCEPT_CULTURES = 300	--How much the AI values having much development in accepted cultures.
---NDefines.NAI.DEFICIT_SPENDING_MIN_MONTHS_PEACETIME = 50	--Same as DEFICIT_SPENDING_MIN_MONTHS, but during peacetime, no rebels and no war exhaustion
+NDefines.NAI.DEFICIT_SPENDING_MIN_MONTHS_PEACETIME = 50	--Same as DEFICIT_SPENDING_MIN_MONTHS, but during peacetime, no rebels and no war exhaustion
 
 NDefines.NAI.DIPLOMATIC_ACTION_GUARANTEE_POWERBALANCE_FACTOR = 25
 NDefines.NAI.POWERFUL_ALLY_PENALTY = 60	--Penalty on alliance for them already having a powerful ally if much stronger.
@@ -72,9 +72,9 @@ NDefines.NAI.INVADING_BRAVERY =	1.2	--If (defender strength) / (invader strength
 NDefines.NAI.INVADING_MAX_AWAY_RATIO = 0.5
 NDefines.NAI.HOME_FLEET_MAX_RATIO = 0.3
 NDefines.NAI.MISSIONARY_MAINTENANCE_SHARE = 0.3
-NDefines.NAI.ADVISOR_BUDGET_FRACTION = 0.25 -- AI will spend a maximum of this fraction of monthly income on advisor maintenance
+--NDefines.NAI.ADVISOR_BUDGET_FRACTION = 0.3 -- AI will spend a maximum of this fraction of monthly income on advisor maintenance
 NDefines.NAI.ADVISOR_PROMOTION_AGE_CUTOFF = 45
-NDefines.NAI.ACCEPTABLE_BALANCE_DEFAULT = 1.8
+NDefines.NAI.ACCEPTABLE_BALANCE_DEFAULT = 1.9
 NDefines.NAI.ACCEPTABLE_BALANCE_FRIEND_IN_COMBAT = 0.85
 
 --Note that armies prefer the province with the lowest score. Also applies to homeland prioritization.
@@ -84,15 +84,18 @@ NDefines.NAI.IMPORANT_PROVINCE_THRESHOLD = 0.03 --AI will try to defend province
 NDefines.NAI.ARMY_DISTANCE_SCORE_IMPACT	= 1.0	--Army <-> province distance impact on province evaluation
 NDefines.NAI.BORDER_DISTANCE_SCORE_IMPACT =	8.0	--Border <-> province distance impact on province evaluation
 NDefines.NAI.MIN_FORCE_LIMIT_SHARE_REGION_ASSIGN = 0.1	--AI will only assign armies larger that this to a region
---NDefines.NAI.ASSIMILATION_INTEREST_AMOUNT_FACTOR = 25 --Influence on assimilation interest from number of provinces left to conquer
+--NDefines.NAI.ASSIMILATION_INTEREST_AMOUNT_FACTOR = 10 --Influence on assimilation interest from number of provinces left to conquer
 --NDefines.NAI.PURSUE_DISTANCE = 100
 
-
+NDefines.NAI.PEACE_WAR_DIRECTION_FACTOR = 0.6 -- AI willingness to peace based on who's making gains in the war
+--NDefines.NAI.PEACE_WAR_DIRECTION_WINNING_MULT = 4 -- Multiplies AI emphasis on war direction if it's the one making gains
+--NDefines.NAI.PEACE_STALLED_WAR_THRESHOLD = 5 -- If the warscore has changed by this amount or less in the last year, the war is stalled
 NDefines.NAI.PEACE_OTHER_WAR_FORCE_BALANCE_MULT = 0.33 -- Multiplies the force balance of other countries who are involved in a different war with either side
 NDefines.NAI.PEACE_ALLY_MILITARY_STRENGTH_MULT = 1.0 -- Multiplies PEACE_MILITARY_STRENGTH_FACTOR for allies in a war
-NDefines.NAI.PEACE_ALLY_FORCE_BALANCE_MULT = 1.0 -- Multiplies PEACE_FORCE_BALANCE_FACTOR for allies in a war
-NDefines.NAI.PEACE_ALLY_WAR_DIRECTION_MULT = 1.0 -- Multiplies PEACE_WAR_DIRECTION_FACTOR for allies in a war
-NDefines.NAI.PEACE_FORCE_BALANCE_FACTOR = 0.25 -- AI willingness to peace based on strength estimation of both sides
+NDefines.NAI.PEACE_ALLY_FORCE_BALANCE_MULT = 0.5 -- Multiplies PEACE_FORCE_BALANCE_FACTOR for allies in a war
+NDefines.NAI.PEACE_ALLY_WAR_DIRECTION_MULT = 0.5 -- Multiplies PEACE_WAR_DIRECTION_FACTOR for allies in a war
+--NDefines.NAI.PEACE_ALLY_WARGOAL_MULT = 0.25 -- Multiplies PEACE_WARGOAL_FACTOR for allies in a war
+--NDefines.NAI.PEACE_FORCE_BALANCE_FACTOR = 0.2 -- AI willingness to peace based on strength estimation of both sides
 
 NDefines.NAI.PEACE_TERMS_HUMILIATE_VALUE_MAX = 3.0 --Max AI desire for humiliating its enemy.
 NDefines.NAI.PEACE_TERMS_HUMILIATE_RIVAL_BASE_MULT = 2.0
@@ -103,12 +106,12 @@ NDefines.NAI.PEACE_TERMS_ANNUL_TREATIES_BASE_MULT =	1
 NDefines.NAI.PEACE_TERMS_GOLD_BASE_MULT = 0.2
 NDefines.NAI.PEACE_TERMS_RELEASE_ANNEXED_MAX_MULT =	0.5	--Max AI desire mult for releasing countries.
 NDefines.NAI.PEACE_TERMS_WAR_REPARATIONS_BASE_MULT = 0.5	--AI desire for war reparations through peace.
-NDefines.NAI.PEACE_TERMS_WAR_REPARATIONS_MIN_INCOME_RATIO = 0.5 -- AI only wants war reparations if other country has at least this % of their income
+NDefines.NAI.PEACE_TERMS_WAR_REPARATIONS_MIN_INCOME_RATIO = 1 -- AI only wants war reparations if other country has at least this % of their income
 NDefines.NAI.PEACE_TERMS_PROVINCE_NO_INTEREST_MULT = 0.15  --AI desire for a province is multiplied by this if it is not on their conquest list.
 NDefines.NAI.PEACE_TERMS_PROVINCE_CORE_MULT = 5.0 -- AI desire for a province is multiplied by this if it has a core on it
 NDefines.NAI.PEACE_TERMS_RELEASE_ANNEXED_HRE_MULT = 3.0 -- AI desire for releasing a country is multiplied by this if both are HRE members
 NDefines.NAI.PEACE_TERMS_CHANGE_GOVERNMENT_BASE_MULT = 100.0 -- only applied if CB is valid for it
-NDefines.NAI.PEACE_TERMS_PROVINCE_OVEREXTENSION_MIN_MULT = 0.85	--AI desire for a province is multiplied by this if it has 99% overextension (not applied to cores).
+NDefines.NAI.PEACE_TERMS_PROVINCE_OVEREXTENSION_MIN_MULT = 0.9	--AI desire for a province is multiplied by this if it has 99% overextension (not applied to cores).
 
 NDefines.NAI.ESTATE_PRIVILEGE_REVOKE_THRESHOLD = 5.1
 NDefines.NAI.ESTATE_PRIVILEGE_GRANT_THRESHOLD = 7.5
@@ -121,7 +124,7 @@ NDefines.NAI.UPGRADE_CENTER_OF_TRADE_BASE_AI_DESIRE = 75.0				-- AI scoring for 
 NDefines.NAI.UPGRADE_CENTER_OF_TRADE_AI_POWER_DESIRE = 2.0				-- AI scoring for upgrade centers of trade, division on the amount of trade power AI has in node
 
 NDefines.NAI.TRADE_COMPANY_INVESTMENT_COST_THRESHOLD = 1.1 -- How many times the cost of the investment must be in the treasury to consider buying it
-NDefines.NAI.REPAY_LOAN_BASE_AI_DESIRE = 100	-- AI scoring for repaying loans, multiplied by (MAX(budget - other loans, 0) * number_of_loans)/cost
+NDefines.NAI.REPAY_LOAN_BASE_AI_DESIRE = 1000	-- AI scoring for repaying loans, multiplied by (MAX(budget - other loans, 0) * number_of_loans)/cost
 
 
 --NDefines.NAIEconomy.BUDGETING_ADJUSTMENT_STEP = 0.1			-- Amount in percent to increase/decrease spending on budget posts when adjusting due to surplus/deficit
@@ -141,7 +144,7 @@ NDefines.NAIEconomy.FORT_FRACTION_MILITARIST = 1.25
 NDefines.NAIEconomy.FORT_FRACTION_CAPITALIST = 0.25
 --MISSIONARY_FRACTION = 0.15
 NDefines.NAIEconomy.STATE_MAINTENANCE_FRACTION = 0.25
-NDefines.NAIEconomy.LOAN_REPAYMENT_SAVINGS_PRIORITY = 2
+NDefines.NAIEconomy.LOAN_REPAYMENT_SAVINGS_PRIORITY = 2.5
 --SUBSIDY_PRIORITY_CONSIDERATION_THRESHOLD = 50.0
 --SUBSIDY_DESIRE_TO_PRIORITY_RATIO = 0.05
 
