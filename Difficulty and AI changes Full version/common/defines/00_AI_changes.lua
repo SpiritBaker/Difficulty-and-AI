4877263d6fa7f88df5d5ee77dbb3e0be9c64f51f
SpiritBaker <34450337+Ivanjpeg@users.noreply.github.com>
2020-09-15 20:36:23 +0600

    Importing CK3 terrain
    Added 5 new terrain types
    Replacing majority of base game terrain with CK3 versions.
    Adjusting terrain, climate. Only America was not changed yet.
    Moving Turkish culture into a separate Anatolian group. As a soft nerf to Otto blob.
    Many AI adjesments, to better adapt AI to 1.30.4.

--- Difficulty and AI changes Full version/common/defines/00_AI_changes.lua
+++ Difficulty and AI changes Full version/common/defines/00_AI_changes.lua
@@ -128,10 +128,10 @@ NDefines.NAI.ESTATE_MAX_WANTED_INFLUENCE = 85.0
 NDefines.NAI.ESTATE_MIN_WANTED_CROWNLAND = 20.0
 NDefines.NAI.ESTATE_MAX_PRIVILEDGES = 4
 
-NDefines.NAI.UPGRADE_CENTER_OF_TRADE_BASE_AI_DESIRE = 75.0				-- AI scoring for upgrade centers of trade, multiplied by budget/cost
-NDefines.NAI.UPGRADE_CENTER_OF_TRADE_AI_POWER_DESIRE = 2.5				-- AI scoring for upgrade centers of trade, division on the amount of trade power AI has in node
+NDefines.NAI.UPGRADE_CENTER_OF_TRADE_BASE_AI_DESIRE = 25.0				-- AI scoring for upgrade centers of trade, multiplied by budget/cost
+NDefines.NAI.UPGRADE_CENTER_OF_TRADE_AI_POWER_DESIRE = 4				-- AI scoring for upgrade centers of trade, division on the amount of trade power AI has in node
 
-NDefines.NAI.TRADE_COMPANY_INVESTMENT_COST_THRESHOLD = 1.1 -- How many times the cost of the investment must be in the treasury to consider buying it
+NDefines.NAI.TRADE_COMPANY_INVESTMENT_COST_THRESHOLD = 2 -- How many times the cost of the investment must be in the treasury to consider buying it
 NDefines.NAI.REPAY_LOAN_BASE_AI_DESIRE = 10	-- AI scoring for repaying loans, multiplied by (MAX(budget - other loans, 0) * number_of_loans)/cost
 
 
