1e18d671683c9df9220415e0a91e53d3f3bc8d54
Svatopluk1 <88267989+Svatopluk1@users.noreply.github.com>
2023-08-23 15:40:48 +0000

    Create no_institutions_from_dev.lua
    Zero instantaneous institution growth from clicking the dev button. Natural spread (from neighboring provinces, etc) is not affected by this change, and province development still scales the rate of natural spread.

---
+++ common/defines/no_institutions_from_dev.lua
@@ -0,0 +1,3 @@
+NDefines.NCountry.INSTITUTION_BONUS_FROM_IMP_DEVELOPMENT = 0 -- 5 in vanilla.
+NDefines.NCountry.INSTITUTION_CAP_IMP_DEVELOPMENT = 0 -- 10 in vanilla.
+NDefines.NCountry.INSTITUTION_BASE_IMP_DEVELOPMENT = 0 -- 30 in vanilla.
